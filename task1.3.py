4e6576a562d2c2519a619583a9b987aad974c186
veradrr <druzhok008@gmail.com>
2024-11-01 23:55:47 +0300

    Add files via upload

---
+++ task1.3.py
@@ -0,0 +1,17 @@
+
+def find(list1, item):
+    if item in list1:
+        index = list1.index(item)
+        return index
+    else:
+        return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = find(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
